6ab2b9ae2c6b9f9026ef46d986bb64ec7822826f
user <user@C1011>
2022-04-14 14:54:43 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp13/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+
+                int n = int.Parse(textBox1.Text);
+               
+
+
+                double s = 6* n*n;
+            double v = n * n * n;
+                
+
+                 label1.Text = ($"Площадь  {s} ");
+            label2.Text = ($"Объём  {v} ");
+
+
+        }
+    }
+}
